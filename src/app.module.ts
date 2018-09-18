9b5b3a492afee9e4385322791f2aad33d938e7a0
Daniel Huisman <daniel@huisman.me>
2018-09-18 23:27:04 +0200

    Add GraphQL support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import {Module} from '@nestjs/common';
+import {GraphQLModule} from '@nestjs/graphql';
+
 import {AppController} from './app.controller';
 
 @Module({
-    imports: [],
+    imports: [
+        GraphQLModule.forRoot({
+            typePaths: ['./**/*.graphql'],
+            installSubscriptionHandlers: true
+
+            // TODO: set these is production environment
+            // debug: false,
+            // playground: false
+        })
+    ],
     controllers: [AppController],
     providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import {NestFactory} from '@nestjs/core';
+import helmet from 'helmet';
+
 import {AppModule} from './app.module';
 
 (async () => {
     const app = await NestFactory.create(AppModule);
+
+    app.use(helmet());
+
     await app.listen(5000);
 })();
